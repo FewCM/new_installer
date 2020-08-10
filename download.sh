865b39e94e1f98963325fb60aa0e834371b6b87e
FewCM <47686483+FewCM@users.noreply.github.com>
2020-08-11 05:29:35 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-curl -L https://github.com/slayerizer/arch_installer/archive/master.zip --output scripts.zip
+curl -L https://github.com/FewCM/new_installer/archive/master.zip --output scripts.zip
 pacman -Sy --noconfirm unzip
 unzip scripts.zip
-cd arch_installer-master
+cd new_installer-master
 chmod +x *.sh
 ./install.sh
